1eee82f28965c5214d4bfb95a74721d1ec4cf724
Geming400 <Geming400@gmail.com>
2024-10-01 20:05:45 +0200

    Updated according to 2.206 (stupid version)

--- src/actions/RemoveSelected.cpp
+++ src/actions/RemoveSelected.cpp
@@ -1,6 +1,7 @@
 #include "RemoveSelected.hpp"
 #include "ActionResponse.hpp"
 
+#include <Geode/Geode.hpp>
 #include <Geode/utils/cocos.hpp>
 #include <Geode/loader/Log.hpp>
 #include <Geode/binding/GameObject.hpp>
@@ -13,10 +14,15 @@ bool RemoveSelectedObjects::isValid(const matjson::Object& j)
 
 ActionResponse RemoveSelectedObjects::run(LevelEditorLayer* editor, const matjson::Object& j)
 {
+    auto m_objects = editor->m_objects;
+
     EditorUI* ui = editor->m_editorUI;
-    for (GameObject* obj : geode::cocos::CCArrayExt<GameObject*>(ui->getSelectedObjects()))
+    for(GameObject* obj : geode::cocos::CCArrayExt<GameObject*>(ui->getSelectedObjects()))
     {
-        ui->deleteObject(obj, false);
+        m_objects->removeObject(obj);
+        editor->removeChild(obj, true);
+        obj->cleanup();
     }
+
     return ActionResponse::make_success();
 }
